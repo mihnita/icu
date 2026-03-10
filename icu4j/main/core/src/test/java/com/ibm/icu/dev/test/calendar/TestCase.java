bd05bd24b1b4c4b6698b47adb7a10f0082212d19
Mihai Nita <nmihai_2000@yahoo.com>
2026-03-10 14:03:03 -0700

    ICU-23247 Fix Errorprone EqualsHashCode error

--- icu4j/main/core/src/test/java/com/ibm/icu/dev/test/calendar/TestCase.java
+++ icu4j/main/core/src/test/java/com/ibm/icu/dev/test/calendar/TestCase.java
@@ -14,6 +14,7 @@ import com.ibm.icu.util.GregorianCalendar;
 import com.ibm.icu.util.SimpleTimeZone;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Objects;
 
 /**
  * A pseudo <code>Calendar</code> that is useful for testing new calendars. A <code>TestCase</code>
@@ -247,7 +248,19 @@ public class TestCase {
      */
     @Override
     public boolean equals(Object obj) {
-        return time == ((Calendar) obj).getTime().getTime();
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof TestCase)) {
+            return false;
+        }
+        TestCase other = (TestCase) obj;
+        return time == other.getTime().getTime();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(time);
     }
 
     protected static final int ONE_SECOND = 1000;
